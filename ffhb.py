95d32821046ef681e6a5d4fc3883783d8f5b74be
Martin Geno <geno@fireorbit.de>
2015-12-05 21:26:20 +0100

    first vpn status

--- ffhb.py
+++ ffhb.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
 import urllib2
@@ -8,6 +8,7 @@ import math
 
 NODELIST_URL = "http://downloads.bremen.freifunk.net/data/nodelist.json"
 NODES_URL = "http://downloads.bremen.freifunk.net/data/nodes.json"
+STATUS_URL = "http://status.ffhb.tk/data/merged.json"
 
 
 def get_json(url):
@@ -54,7 +55,7 @@ def ffhb_status(bot, trigger):
     messages = []
     total_count = 0.0
     online_count = 0.0
-    client_count = 0.0    
+    client_count = 0.0
 
     for node in nodes["nodes"]:
         if node["status"]["online"]:
@@ -134,8 +135,46 @@ def ffhb_highscore(bot, trigger):
     messages = []
 
     for idx in range(0,5):
-	 messages.append("{}: {} ({})".format(idx+1, shorter(nodes[idx]["name"]), nodes[idx]["status"]["clients"]))
+        messages.append("{}: {} ({})".format(idx+1, shorter(nodes[idx]["name"]), nodes[idx]["status"]["clients"]))
 
+    send_messages(bot, command_name, messages)
+
+@sopel.module.commands('gateway', 'vpn')
+def ffhb_gateway(bot, trigger):
+    command_name = "vpn"
+    server = trigger.group(2).lower()
+    status_json = get_json(STATUS_URL)
+    status = {}
+    count = 0
+    for data in status_json:
+        for vpn in data["vpn-servers"]:
+            if vpn["name"] == server:
+                if vpn["name"] not in status:
+                    status[vpn["name"]] = {"ntp":{"ipv6":0,"ipv4":0},"addresses":{"ipv6":0,"ipv4":0},"dns":{"ipv6":0,"ipv4":0},"uplink":{"ipv6":0,"ipv4":0}}
+                count+=1
+                status[vpn["name"]]["ntp"]["ipv4"]+= vpn["ntp"][0]["ipv4"]
+                status[vpn["name"]]["ntp"]["ipv6"]+= vpn["ntp"][0]["ipv6"]
+                status[vpn["name"]]["addresses"]["ipv4"]+= vpn["addresses"][0]["ipv4"]
+                status[vpn["name"]]["addresses"]["ipv6"]+= vpn["addresses"][0]["ipv6"]
+                status[vpn["name"]]["dns"]["ipv4"]+= vpn["dns"][0]["ipv4"]
+                status[vpn["name"]]["dns"]["ipv6"]+= vpn["dns"][0]["ipv6"]
+                status[vpn["name"]]["uplink"]["ipv4"]+= vpn["uplink"][0]["ipv4"]
+                status[vpn["name"]]["uplink"]["ipv6"]+= vpn["uplink"][0]["ipv6"]
+    for vpn in status:
+        status[vpn]["ntp"]["ipv4"] = status[vpn]["ntp"]["ipv4"]/count
+        status[vpn]["ntp"]["ipv6"] = status[vpn]["ntp"]["ipv6"]/count
+        status[vpn]["addresses"]["ipv4"] = status[vpn]["addresses"]["ipv4"]/count
+        status[vpn]["addresses"]["ipv6"] = status[vpn]["addresses"]["ipv6"]/count
+        status[vpn]["dns"]["ipv4"] = status[vpn]["dns"]["ipv4"]/count
+        status[vpn]["dns"]["ipv6"] = status[vpn]["dns"]["ipv6"]/count
+        status[vpn]["uplink"]["ipv4"] = status[vpn]["uplink"]["ipv4"]/count
+        status[vpn]["uplink"]["ipv6"] = status[vpn]["uplink"]["ipv6"]/count
+    messages = []
+    messages.append("Into VPN-Server: {}".format(server))
+    messages.append("NTP: (IPv4: {}, IPv6: {})".format(status[server]["ntp"]["ipv4"],status[server]["ntp"]["ipv6"]))
+    messages.append("ADDR: (IPv4: {}, IPv6: {})".format(status[server]["addresses"]["ipv4"],status[server]["addresses"]["ipv6"]))
+    messages.append("DNS: (IPv4: {}, IPv6: {})".format(status[server]["dns"]["ipv4"],status[server]["dns"]["ipv6"]))
+    messages.append("Uplink: (IPv4: {}, IPv6: {})".format(status[server]["uplink"]["ipv4"],status[server]["uplink"]["ipv6"]))
     send_messages(bot, command_name, messages)
 
 def shorter(s, length=27, ext="..."):
