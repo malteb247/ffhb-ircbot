6e565f3d7e70eed4f474441847cd6791fa253263
ec8or <ec8or@posteo.de>
2015-12-16 00:47:54 +0100

    seperated vpn and node processing to reduce amount of if statements. More paramater validation to skip download of json files if the arguments are wrong.

--- ffhb.py
+++ ffhb.py
@@ -6,7 +6,6 @@ import json
 import sopel.module
 import threading
 import time
-import math
 
 NODELIST_URL = "http://downloads.bremen.freifunk.net/data/nodelist.json"
 NODES_URL = "http://downloads.bremen.freifunk.net/data/nodes.json"
@@ -14,14 +13,20 @@ STATUS_OVERALL = "http://status.ffhb.tk/data/overall.json"
 STATUS_URL_RELOAD_MINUTES = 5
 CHANNEL = "#ffhb_gelaber"
 
-#CACHE-DB
-status_db = {}
+# VPN-Server status cache.
+status_cache = {}
+
 
 def setup(bot):
-    def monitor(bot):
+    """ Is called on start by sopel
+
+    :param bot: bot to configure
+    """
+
+    def monitor(inner_bot):
         time.sleep(5)
         while True:
-            ffhb_gateway(bot, None, CHANNEL)
+            ffhb_gateway(inner_bot, None, CHANNEL)
             time.sleep(STATUS_URL_RELOAD_MINUTES * 60)
 
     t = threading.Thread(target=monitor, args=(bot,))
@@ -100,10 +105,14 @@ def ffhb_node(bot, trigger):
     :param trigger: command that triggered
     """
     command_name = "node"
+
+    if trigger.group(2) is None:
+        send_messages(bot, command_name, ["Bitte gib einen Knotennamen oder -id als ersten Parameter an."])
+        return
+
     search_text = trigger.group(2).lower()
-    info = get_json(NODES_URL)
-    messages = ["Zu diesem Knoten liegen keine Infos vor."]
     found_node = None
+    info = get_json(NODES_URL)
 
     for n in info["nodes"]:
         node = info["nodes"][n]
@@ -112,46 +121,60 @@ def ffhb_node(bot, trigger):
             break
 
     if found_node is None:
-        send_messages(bot, command_name, messages)
+        send_messages(bot, command_name, ["Zu diesem Knoten liegen keine Infos vor."])
         return
 
+    if "vpn" in found_node["nodeinfo"]:
+        send_messages(bot, command_name, get_messages_vpn(found_node))
+    else:
+        send_messages(bot, command_name, get_messages_node(found_node))
+
+
+def get_messages_vpn(found_node):
     messages = []
     node_info = found_node["nodeinfo"]
-    auto_update = None
 
-    # gateways have no autoupdater
-    if "autoupdater" in node_info["software"]:
-        if node_info["software"]["autoupdater"]["enabled"]:
-            auto_update = "Auto-update {}".format(node_info["software"]["autoupdater"]["branch"])
-        else:
-            auto_update = "Auto-update AUS"
-
-    messages.append("{} ({})".format(node_info["hostname"],
-                                     node_info["node_id"]))
-
-    if "owner" in found_node["nodeinfo"]:
-        messages.append("Kontakt  : " + node_info["owner"]["contact"])
-    else:
-        messages.append("Kontakt  : n/a")
+    messages.append("{} ({})".format(node_info["hostname"], node_info["node_id"]))
 
     status = "OFFLINE"
-
     if found_node["flags"]["online"]:
         status = "online ({} clients)".format(found_node["statistics"]["clients"])
 
-    messages.append("Status   : " + status)
+    messages.append("Status   : {} ({})".format(status, found_node["lastseen"]))
+    messages.append("Hinweis  : Dies ist ein VPN Server.")
+
+    return messages
+
+
+def get_messages_node(found_node):
+    messages = []
+    node_info = found_node["nodeinfo"]
+
+    auto_update = "Auto-update AUS"
+
+    if node_info["software"]["autoupdater"]["enabled"]:
+        auto_update = "Auto-update {}".format(node_info["software"]["autoupdater"]["branch"])
+
+    messages.append("{} ({})".format(node_info["hostname"], node_info["node_id"]))
+
+    contact = "n/a"
+    if "owner" in found_node["nodeinfo"]:
+        contact = node_info["owner"]["contact"]
+    messages.append("Kontakt  : " + contact)
+
+    status = "OFFLINE"
+    if found_node["flags"]["online"]:
+        status = "online ({} clients)".format(found_node["statistics"]["clients"])
+
+    messages.append("Status   : {} ({})".format(status, found_node["lastseen"]))
 
     if "hardware" in node_info:
         messages.append("Model    : " + node_info["hardware"]["model"])
 
-    if auto_update is not None:
-        messages.append("Firmware : {} ({})".format(node_info["software"]["firmware"]["release"], auto_update))
-    else:
-        messages.append("Firmware : {}".format(node_info["software"]["firmware"]["release"]))
-
+    messages.append("Firmware : {} ({})".format(node_info["software"]["firmware"]["release"], auto_update))
     messages.append("http://bremen.freifunk.net/meshviewer/#!v:m;n:" + node_info["node_id"])
 
-    send_messages(bot, command_name, messages)
+    return  messages
 
 
 @sopel.module.commands('top')
@@ -180,13 +203,14 @@ def ffhb_gateway(bot, trigger, to=None):
     """
     command_name = "vpn"
     limit = 50
+    server_name_postfix = ".bremen.freifunk.net"
     # Selector
     if trigger is not None and trigger.group(2) is not None:
         vpn_server = trigger.group(2).lower()
     else:
         vpn_server = "all"
     if "." not in vpn_server:
-        vpn_server += ".bremen.freifunk.net"
+        vpn_server += server_name_postfix
 
     services = ["ntp", "addresses", "dns", "uplink"]
     status = get_json(STATUS_OVERALL)['vpn-servers']
@@ -195,27 +219,33 @@ def ffhb_gateway(bot, trigger, to=None):
     for vpn in status:
         if vpn_server == vpn or "all" in vpn_server:
             for service in services:
-                changed = False
-                if vpn not in status_db:
-                    status_db[vpn] = {}
-                    changed = True
-                if service not in status_db[vpn]:
-                    status_db[vpn][service] = {"ipv4":0,"ipv6":0}
+                has_changes = False
 
+                if vpn not in status_cache:
+                    status_cache[vpn] = {}
+                    has_changes = True
 
-                if status[vpn][service]["ipv4"]["percent-good"] != status_db[vpn][service]["ipv4"]:
-                    status_db[vpn][service]["ipv4"] = status[vpn][service]["ipv4"]["percent-good"]
-                    changed = True
+                if service not in status_cache[vpn]:
+                    status_cache[vpn][service] = {"ipv4": 0, "ipv6": 0}
 
-                if status[vpn][service]["ipv6"]["percent-good"] != status_db[vpn][service]["ipv6"]:
-                    status_db[vpn][service]["ipv6"] = status[vpn][service]["ipv6"]["percent-good"]
-                    changed = True
+                ipv4_percent = status[vpn][service]["ipv4"]["percent-good"]
+                ipv6_percent = status[vpn][service]["ipv6"]["percent-good"]
 
-                if (status[vpn][service]["ipv4"]["percent-good"] < limit or status[vpn][service]["ipv6"]["percent-good"] < limit) and (changed or to is None):
-                    messages.append("{} - {}: (IPv4: {}%, IPv6: {}%)".format(vpn,
-                                                                           service,
-                                                                           status[vpn][service]["ipv4"]["percent-good"],
-                                                                           status[vpn][service]["ipv6"]["percent-good"]))
+                if ipv4_percent != status_cache[vpn][service]["ipv4"]:
+                    status_cache[vpn][service]["ipv4"] = ipv4_percent
+                    has_changes = True
+
+                if ipv6_percent != status_cache[vpn][service]["ipv6"]:
+                    status_cache[vpn][service]["ipv6"] = ipv6_percent
+                    has_changes = True
+
+                msg_format = "{} - {}: (IPv4: {}%, IPv6: {}%)"
+
+                if (ipv4_percent < limit or ipv6_percent < limit) and (has_changes or to is None):
+                    messages.append(msg_format.format(vpn,
+                                                      service.replace(server_name_postfix, ""),
+                                                      ipv4_percent,
+                                                      ipv6_percent))
     if len(messages) > 0:
         if to is None:
             send_messages(bot, command_name, messages)
